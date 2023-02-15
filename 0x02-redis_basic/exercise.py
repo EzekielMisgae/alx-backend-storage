8ddabd7c5da17f2e052645a15355a9ea246119aa
ezekielalene <ezkielmisgae@gmail.com>
2023-02-16 01:49:23 +0300

    Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Redis module, Writing strings to Redis
+Reading from Redis and recovering original type
+Incrementing values, storing lists, Retrieving lists
+"""
+import redis
+import uuid
+from typing import Union, Callable, Optional
+from functools import wraps
+
+
+class Cache():
+    """
+    Store instance of Redis client as private variable _redis
+    Flush the instance using flushdb
+    """
+    def __init__(self):
+        """
+        Prototype: def __init__(self):
+        Store instance of Redis client as private variable _redis
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
